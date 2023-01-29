37b60167192d94463a0550a7ca2d56db01b01d1f
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-01-29 21:41:32 +0200

    tray for new

--- graphql/index.js
+++ graphql/index.js
@@ -1,17 +1,61 @@
 let arc = require('@architect/functions')
 let {ApolloServer, gql} = require('apollo-server-lambda')
+import resolvers  from "./resolvers"
 
 let typeDefs = gql`
-  type Query {
-    hello: String
-  }
+      type Price {
+        currency: Currency!,
+        amount: Float!
+    }
+
+    type Attribute {
+        displayValue: String,
+        value: String,
+        id: String!
+    }
+
+    type AttributeSet {
+        id: String!,
+        name: String,
+        type: String,
+        items: [Attribute]
+    }
+
+    type Product {
+        id: String!,
+        name: String!,
+        inStock: Boolean,
+        gallery: [String],
+        description: String!,
+        category: String!,
+        attributes: [AttributeSet]
+        prices: [Price!]!,
+        brand: String!
+    }
+
+    type Category {
+        name: String,
+        products: [Product]!
+    }
+
+    type Currency {
+        label: String!,
+        symbol: String!
+    }
+
+    input CategoryInput {
+        title: String!
+    }
+
+    type Query {
+        categories: [Category],
+        category(input: CategoryInput): Category,
+        product(id: String!): Product,
+        currencies: [Currency]
+    }
 `
 
-let resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-}
+let resolvers =  resolvers
 
 let server = new ApolloServer({typeDefs, resolvers})
 let handler = server.createHandler()
