783131aabdc8719c4bb01056aa63c5d50362bfda
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-01-30 16:14:58 +0200

    tray

--- graphql/index.js
+++ graphql/index.js
@@ -1,5 +1,5 @@
-// let arc = require('@architect/functions')
-// let {ApolloServer, gql} = require('apollo-server-lambda')
+let arc = require('@architect/functions')
+let {ApolloServer, gql} = require('apollo-server-lambda')
 // import resolvers  from "./resolvers"
 // var resolvers_1 = __importDefault(require("./resolvers"));
 // let resolvers = require("./resolvers/index.js")
@@ -59,35 +59,37 @@
 
 
 
-// let server = new ApolloServer({typeDefs,resolvers_1})
-// let handler = server.createHandler()
+let server = new ApolloServer({typeDefs,resolvers_1})
+let handler = server.createHandler()
 
-// exports.handler = function(event, context, callback) {
-//   let body = arc.http.helpers.bodyParser(event)
-//   console.log(body)
-//   console.log(resolvers)
-//   // Support for AWS HTTP API syntax
-//   event.httpMethod = event.httpMethod
-//     ? event.httpMethod
-//     : event.requestContext.http.method
-//   // Also support hte HTTP syntax...
-//   event.path = event.rawPath
-//   // Body is now parsed, re-encode to JSON for Apollo
-//   event.body = JSON.stringify(body)
-//   handler(event, context, callback)
-// }
-var __importDefault = (this && this.__importDefault) || function (mod) {
-  return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var apollo_server_1 = require("apollo-server");
-var schema_1 = __importDefault(require("./schema"));
-var resolvers_1 = __importDefault(require("./resolvers"));
-var server = new apollo_server_1.ApolloServer({
-  typeDefs: schema_1.default,
-  resolvers: resolvers_1.default
-});
-server.listen().then(function (_a) {
-  var url = _a.url;
-  console.log("\uD83D\uDE80  Server ready at " + url);
-});
+exports.handler = function(event, context, callback) {
+  let body = arc.http.helpers.bodyParser(event)
+  console.log(body)
+  console.log(resolvers)
+  // Support for AWS HTTP API syntax
+  event.httpMethod = event.httpMethod
+    ? event.httpMethod
+    : event.requestContext.http.method
+  // Also support hte HTTP syntax...
+  event.path = event.rawPath
+  // Body is now parsed, re-encode to JSON for Apollo
+  event.body = JSON.stringify(body)
+  handler(event, context, callback)
+}
+
+
+// var __importDefault = (this && this.__importDefault) || function (mod) {
+//   return (mod && mod.__esModule) ? mod : { "default": mod };
+// };
+// Object.defineProperty(exports, "__esModule", { value: true });
+// var apollo_server_1 = require("apollo-server");
+// var schema_1 = __importDefault(require("./schema"));
+// var resolvers_1 = __importDefault(require("./resolvers"));
+// var server = new apollo_server_1.ApolloServer({
+//   typeDefs: schema_1.default,
+//   resolvers: resolvers_1.default
+// });
+// server.listen().then(function (_a) {
+//   var url = _a.url;
+//   console.log("\uD83D\uDE80  Server ready at " + url);
+// });
